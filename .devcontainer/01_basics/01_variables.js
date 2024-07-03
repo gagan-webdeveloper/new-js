93b9b258826b3520004cdb8530c461cf3853103f
gagan-webdeveloper <gaganyadav530418@gmail.com>
2024-07-03 05:39:21 +0000

    variables , datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID=14456;
+let name="gagan";
+accountEmail = "yadavgagan530418@gmail.com";
+var password = "gaganyadav";
+let accountState;
+accountCity="chennai";
+
+// accountID = 23; //not allowed as account id is declared as constant in the beginning
+
+accountEmail = "gaganyadav530418@gmail.com";
+password = "yadavgagan";
+accountCity = "banglore";
+
+console.table([accountEmail , accountCity , accountID, password, accountState]);
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //use all js code as newer version
+
+// alert(3+3) // it will not work as that of html , because we are using node js not browser..
+
+/*
+
+number => 2 to power53;
+strings => "";
+boolean => true/false;
+bigint => used for storing bigger values , in websites such as stock market etc..
+undefined => variable declared , but value is not assigned..    (typeof udefined is undefined)
+null => representation of the empty value..                 (typeof null is object )
+
+*/
+
+console.log(typeof undefined);
