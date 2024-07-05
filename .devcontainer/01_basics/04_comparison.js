ef8a400cd8d7b47d944f31178ef41c52aabbae12
gagan-webdeveloper <gaganyadav530418@gmail.com>
2024-07-05 03:08:39 +0000

    comparison js

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+// console.log(1<=2);
+// console.log(1>2);
+// console.log("2">1);
+// console.log("1"<2);
+// console.log(1!=2);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);           //it will come true because comparison converts null value to zero....
+
+//equality check and comparison are both different things..
