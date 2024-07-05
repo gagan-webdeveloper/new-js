53c12ff35756ed15565cb1ce91915e5862b17cb1
gagan-webdeveloper <gaganyadav530418@gmail.com>
2024-07-05 11:29:32 +0000

    number and math

---
+++ .devcontainer/01_basics/07_num_and_math.js
@@ -0,0 +1,25 @@
+let score = 400;
+console.log(score);
+
+
+let balance = new Number(100);      //here we can particularly fix the datatype to the number..
+console.log(balance);
+
+console.log(balance.toString());        //here we can convert the data type to the string!!
+console.log(balance.toFixed(2));    //here we can decide how many zeroes to come after decimal....
+
+
+let newNumber = 133.8324;
+// console.log(newNumber.toPrecision(4));      //it rounds off the value according to the given digits ... priority starts from the left of the decimal...
+
+
+
+let hundreds = 10000000;
+// console.log(hundreds.toLocaleString());     //here commas will appear after 3 digits....
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++++++++
+
